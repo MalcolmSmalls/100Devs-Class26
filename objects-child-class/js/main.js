1ff9a9610d810f82f4762dca107f817e24890e8a
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-07 09:24:14 -0700

    Made ShadyRecords class instead and made Griselda the subclass/child

--- objects-child-class/js/main.js
+++ objects-child-class/js/main.js
@@ -1,9 +1,21 @@
 //Create an a class and extend it - Can be anything you would like it to be! 
 
-class Griselda {
+class ShadyRecords {
     constructor ( name, specialPower, age){
         this.artistName = name
         this.specialPower = specialPower
         this.age = age
     }
 }
+class Griselda extends ShadyRecords{
+    constructor( name, specialPower, age, signature){
+        super(artistName)
+        super(specialPower)
+        super(age)
+        this.signature = signature
+        finisher(){
+            alert(`${Griselda.artistName}: ${Griselda.signature}`)
+        }
+    }
+}
+
