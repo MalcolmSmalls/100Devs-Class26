41ab67f4c2531044881f75a9e56e9b762f524300
Malcolm Smalls <smallsbeats@gmail.com>
2022-08-07 09:10:52 -0700

    Added Griselda object

---
+++ objects-child-class/js/main.js
@@ -0,0 +1,9 @@
+//Create an a class and extend it - Can be anything you would like it to be! 
+
+class Griselda {
+    constructor ( name, specialPower, age){
+        this.artistName = name
+        this.specialPower = specialPower
+        this.age = age
+    }
+}
